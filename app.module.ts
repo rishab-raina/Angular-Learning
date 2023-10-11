15bb7d35bd916839dcd002d5f090f3895b36c9df
rishab-raina <31505431+rishab-raina@users.noreply.github.com>
2023-10-11 11:22:59 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { TranslateService } from './translate.service';
 import { TranslatePipe } from './translate.pipe';
 import { AppSettings } from '../AppSettings';
+import { CanActivateRouteGuard } from './can-activate-route.guard';
 
 export function setupTranslateFactory(service: TranslateService): Function {
   return () => service.use(AppSettings.language);
