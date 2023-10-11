39f0427a857d06365ec5fc6ac56c2d1f9e7ab288
rishab-raina <31505431+rishab-raina@users.noreply.github.com>
2023-10-12 00:10:57 +0530

    Create canDeactivate-guard.ts

---
+++ canDeactivate-guard.ts
@@ -0,0 +1,26 @@
+ import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router/src/utils/preactivation';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RegisterComponent } from './register.component';
+ 
+@Injectable()
+export class DeactivateGuard implements CanDeactivate 
+{
+    component: Object;
+    route: ActivatedRouteSnapshot;
+ 
+   constructor(){
+   }
+ 
+   canDeactivate(component:RegisterComponent,
+                route: ActivatedRouteSnapshot, 
+                state: RouterStateSnapshot,
+                nextState: RouterStateSnapshot) : Observable<boolean> | Promise<boolean> | boolean {
+        
+        return component.canExit();
+ 
+  }
+  
+}
+ 
